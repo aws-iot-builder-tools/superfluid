9244c85559a56a282d3676dc7b56427468780879
Tim Mattison <tim@mattison.org>
2022-06-30 23:47:50 -0400

    Added Greengrass deployment info and latest deployment local features

--- src/main/java/com/awslabs/superfluid/commands/Parent.java
+++ src/main/java/com/awslabs/superfluid/commands/Parent.java
@@ -3,11 +3,13 @@ package com.awslabs.superfluid.commands;
 import com.awslabs.superfluid.commands.ec2.Ec2;
 import com.awslabs.superfluid.commands.greeneyes.GreenEyes;
 import com.awslabs.superfluid.commands.greengrass.cloud.GreengrassCloud;
+import com.awslabs.superfluid.commands.greengrass.local.GreengrassLocal;
 import picocli.CommandLine;
 
 @CommandLine.Command(name = "", subcommands = {
         Ec2.class,
         GreengrassCloud.class,
+        GreengrassLocal.class,
         GreenEyes.class
 })
 
