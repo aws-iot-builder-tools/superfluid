02fa192c8a6ebfaf114a4cf2c7608f37d8babd02
Tim Mattison <tim@mattison.org>
2022-06-30 23:44:59 -0400

    Added Greengrass get recipe and show deployment cloud features

--- src/main/java/com/awslabs/superfluid/commands/Parent.java
+++ src/main/java/com/awslabs/superfluid/commands/Parent.java
@@ -2,10 +2,12 @@ package com.awslabs.superfluid.commands;
 
 import com.awslabs.superfluid.commands.ec2.Ec2;
 import com.awslabs.superfluid.commands.greeneyes.GreenEyes;
+import com.awslabs.superfluid.commands.greengrass.cloud.GreengrassCloud;
 import picocli.CommandLine;
 
 @CommandLine.Command(name = "", subcommands = {
         Ec2.class,
+        GreengrassCloud.class,
         GreenEyes.class
 })
 
